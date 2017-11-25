8717be8c03249b826a51e203815f8ca5dea0ffb2
Amir Ingher <amir.ingher26@gmail.com>
2017-11-25 20:56:59 +0200

    more awesome css

--- frontend/src/components/ListItem.js
+++ frontend/src/components/ListItem.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-
+import '../styles/list-page.css';
 const ListItem = ({index}) => {
   return (
     <table>
       <tbody>
-        <tr>
-          <td className="fuel-savings-label"><button>Up</button></td>
+        <tr className='tr-zone'>
+          <td className="fuel-savings-label"><button className='upButton'></button></td>
           <td> Song: {index} </td>
-          <td><button>Down</button></td>
+          <td><button className='downButton'></button></td>
       </tr>
       </tbody>
     </table>
