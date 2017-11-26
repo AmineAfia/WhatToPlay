9557a55b9d6e474e9909548f5bff1a6ffee75710
AmineAfia <afia.amine@gmail.com>
2017-11-26 09:27:21 +0200

    play

--- frontend/src/components/ListItem.js
+++ frontend/src/components/ListItem.js
@@ -8,7 +8,8 @@ function ListItem({index, onUpvoteClick, onDownvoteClick}) {
     <table className='tr-zone'>
       <tbody>
         <tr>
-          <td className="fuel-savings-label"><button className='upButton' type="submit" onClick={onUpvoteClick}></button></td>
+          <th className="fuel-savings-label"><button className='upButton' type="submit" onClick={onUpvoteClick}></button></th>
+          <td>Play</td>
           <td className='song'> {index} </td>
           <td className="dbutt"><button className='downButton' type="submit" onClick={onDownvoteClick}></button></td>
       </tr>
